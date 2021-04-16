ad6da7ae7ed4a8376900b57942b0b48f53cb037e
Ashraful-Hasan10 <azhasan2001@gmail.com>
2021-04-16 10:39:50 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import ReactPlayer from "react-player";
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      inputValue : "",
+      url:""
+    }
+  }
+
+  handleChange = (event)=>{
+    this.setState({inputValue : event.target.value})
+  }
+
+  handleSubmit = (event) =>{
+    event.preventDefault();
+    this.setState({url: this.state.inputValue})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+              <form onSubmit={this.handleSubmit}>
+                <input onChange={this.handleChange} style={{margin:"20px"}} className="form-control" type="text" placeholder="Input the vidoe url" />
+                <button style={{margin:"10px"}} className="btn btn-primary">PLAY VIDEO</button>
+              </form>
+            
+          </div>
+          <ReactPlayer url={this.state.url} controls={true} />
+        </header>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
